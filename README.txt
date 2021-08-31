3772361dfc52d7c68c9ec28565adda9c0736ac0a
Michał Durlak <durlak.m@wp.pl>
2021-08-31 23:06:47 +0200

    #2 - adding code
    Added code to the repository.
    It is working only for twitch.tv site.

---
+++ README.txt
@@ -0,0 +1,7 @@
+How to install:
+
+1. Open Chrome
+2. Open chrome://extensions
+3. Turn on Developer mode
+4. Click Load Unpacked
+5. Choose file that you downloaded from github (FOLDER)
---
+++ content.js
@@ -0,0 +1,17 @@
+var myVar;
+
+function myFunction() {
+  myVar = setTimeout(getBonus, 3000);
+}
+
+function getBonus(){
+    var isAvailable = document.getElementsByClassName('ScCoreButton-sc-1qn4ixc-0 ScCoreButtonSuccess-sc-1qn4ixc-5 fERWGf');
+    if (isAvailable.length > 0){
+        document.getElementsByClassName('ScCoreButton-sc-1qn4ixc-0 ScCoreButtonSuccess-sc-1qn4ixc-5 fERWGf')[0].click()
+        console.log("BONUS CLICKED");
+    } else {
+        console.log("BONUS HIDEN");
+    }
+}
+
+setInterval(myFunction,10000);
